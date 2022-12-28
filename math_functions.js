0c8901bc2ed73c607f6ae6f6b4ea6b602c63c483
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2022-12-28 23:51:29 +0100

    Added the functions

--- math_functions.js
+++ math_functions.js
@@ -1,1 +1,34 @@
+//sum
+function sum(x, y) {
+  return x + y;
+}
 
+//subtract
+funtion subtract(x, y) {
+  return x - y;
+}
+
+//multiply
+function multiply(x, y) {
+  return x * y;
+}
+
+//divide
+function divide(x, y) {
+  return x / y;
+}
+
+//square
+function square(x){
+  return x ** 2;
+}
+
+//cube
+function cube(x) {
+  return x ** 3;
+}
+
+//power
+function pow(x, y) {
+  return x ** y;
+}
