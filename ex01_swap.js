14f1d62aaaacc852424ddbbe754dc76952bef112
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2023-01-05 11:50:30 +0100

    Challenge - Swapping Variables
    A basic challenge presented in the 'JavaScript Fundamental' course

---
+++ ex01_swap.js
@@ -0,0 +1,18 @@
+// Swap the values of a and b
+
+let a = 'red';
+let b = 'blue';
+
+//printing the original values
+
+console.log('Original a: ' + a);
+console.log('Original b: ' + b);
+
+console.log('\n-----SWAP-----');
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log('Swapped a: ' + a);
+console.log('Swapped b: ' + b);
