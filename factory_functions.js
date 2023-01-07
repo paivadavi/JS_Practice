b75c4dbb970422c780960518788001887c44486b
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2023-01-07 22:10:43 +0100

    Practicing factory functions

--- factory_functions.js
+++ factory_functions.js
@@ -1,18 +1,33 @@
-// Practicing creating factory functions
+//Creating a factory function that returns person objects
 
 function createPerson(name, surname, age, country) {
-  return {
-    name,
-    surname,
-    age,
-    country,
-    greet() {
-      console.log("Hello, my name is " + name + " " + surname 
-                  + "I am " + age + " years old and from " + country);
+    return {
+        name,
+        surname,
+        age,
+        country,
+        greet() {
+            console.log("\nHello, my name is " + name + " " + surname 
+                  + " I am " + age + " years old and from " + country);
+        }
     }
-    
-  }
-
-
-
 }
+
+//TESTING THE FUNCTION
+
+const person01 = createPerson("Davi", "Paiva", 22, "Brazil");
+
+const person02 = createPerson("Jane", "Brooks", 34, "USA");
+
+const person03 = createPerson("Mika", "Fields", 25, "Canada");
+
+const person04 = createPerson("Carla", "Sanchez", 19, "Mexico");
+
+const person05 = createPerson("Diogo", "Defante", 29, "Brazil");
+
+person01.greet();
+person02.greet();
+person03.greet();
+person04.greet();
+person05.greet();
+
