3dcf9701d4e6d04f389b33b197140a30b536daa6
Davi Paiva <50916238+paivadavi@users.noreply.github.com>
2023-01-07 22:01:25 +0100

    creating a factory function for a person

--- factory_functions.js
+++ factory_functions.js
@@ -1,1 +1,18 @@
+// Practicing creating factory functions
 
+function createPerson(name, surname, age, country) {
+  return {
+    name,
+    surname,
+    age,
+    country,
+    greet() {
+      console.log("Hello, my name is " + name + " " + surname 
+                  + "I am " + age + " years old and from " + country);
+    }
+    
+  }
+
+
+
+}
